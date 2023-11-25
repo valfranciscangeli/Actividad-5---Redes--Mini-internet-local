afb682a72bf3ac2f689be4f82363bff55b6deef1
valfranciscangeli <val.franciscangeli@gmail.com>
2023-11-25 00:00:43 -0300

    cambios haciendo el informe

--- router.py
+++ router.py
@@ -3,7 +3,7 @@ import socket
 from utils import *
 
 # debug? ==================================================
-debug: bool = True
+debug: bool = True # True para mostrar mensajes de debuggeo
 
 # recibimos los parámetros desde consola =========================================
 argumentos: list = sys.argv
@@ -18,14 +18,12 @@ buff_size = 4096
 if debug:
     print(f"direccion: {direccion_router_actual}, archivo de rutas: {router_rutas}")
 
-# python3 router.py 127.0.0.1 8881 Conf_2_routers/rutas_R1_v1.txt
-# python3 router.py 127.0.0.1 8882 Conf_2_routers/rutas_R2_v1.txt
 
 # creacion del socket ============================================================
 # socket no orientado a conexión
 router = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# unimos el socket a la dirección this_address
+# unimos el socket a la dirección direccion_router_actual
 router.bind(direccion_router_actual)
 
 # esperamos hasta recibir un mensaje ==============================================
@@ -36,13 +34,13 @@ while True:
     # aquí esperaremos hasta recibir algun mensaje
     while True:
         if debug:
-            print("esperando mensaje ....")
+            print("===================== \nesperando mensaje .... \n")
 
         # recibimos el mensaje usando recvfrom
         recv_message, return_address = router.recvfrom(buff_size)
 
         if debug:
-            print("mensaje recibido:", recv_message.decode())
+            print("mensaje recibido:", recv_message.decode(),"\n")
 
         # si recibimos un mensaje saldremos de este ciclo para continuar con el código
         if recv_message != None:
@@ -59,11 +57,11 @@ while True:
         direccion_next_hop = check_routes(router_rutas, (paquete_ip["ip"], paquete_ip["puerto"]))
 
         if direccion_next_hop:
-            print(f"redirigiendo paquete {paquete_ip} con destino final {destination_address} desde {direccion_router_actual} hacia {direccion_next_hop}")
+            print(f"redirigiendo paquete {paquete_ip} con destino final {destination_address} desde {direccion_router_actual} hacia {direccion_next_hop}\n")
             
             # hacemos forward
             fwd_message = create_packet(paquete_ip).encode()
             router.sendto(fwd_message, direccion_next_hop)
             
         else:
-            print(f"No hay rutas hacia {destination_address} para paquete {paquete_ip}")
+            print(f"No hay rutas hacia {destination_address} para paquete {paquete_ip}\n")
--- utils.py
+++ utils.py
@@ -1,19 +1,25 @@
-from cola_circular import  *
+from cola_circular import *
 
-# variables globales: 
-cola_de_rutas = CircularQueue() # al inicio es vacía
+# variables globales:
+debug = False  # True para mostrar los prints de debugging
+cola_de_rutas = CircularQueue()  # al inicio es vacía
 
 # funciones de parsing  ===============================================
+
+
 def create_packet(dict_packet):
     paquete = f'{dict_packet["ip"]},{dict_packet["puerto"]},{dict_packet["mensaje"] }'
     return paquete
 
 
 def parse_packet(IP_packet):
-    recibido = IP_packet.decode().split(",")
-    return {"ip": recibido[0],
-            "puerto": int(recibido[1]),
-            "mensaje": recibido[2]}
+    # recibe un paquete codificado
+    IP_packet = IP_packet.decode()
+    IP_packet = IP_packet.rstrip("\n")
+    IP_packet = IP_packet.split(",")
+    return {"ip": IP_packet[0],
+            "puerto": int(IP_packet[1]),
+            "mensaje": IP_packet[2]}
 
 
 # tests:
@@ -21,15 +27,18 @@ IP_packet_v1 = "127.0.0.1,8881,hola".encode()
 parsed_IP_packet = parse_packet(IP_packet_v1)
 IP_packet_v2_str = create_packet(parsed_IP_packet)
 IP_packet_v2 = IP_packet_v2_str.encode()
-# print("IP_packet_v1 == IP_packet_v2 ? {}".format(IP_packet_v1 == IP_packet_v2))
+assert IP_packet_v1 == IP_packet_v2
+if debug:
+    print("IP_packet_v1 == IP_packet_v2 ? {}".format(
+        IP_packet_v1 == IP_packet_v2))
 
 
 # funciones para trabajar los txt ===============================================
 
 
-def leer_archivo(nombre_archivo):
+def leer_archivo(archivo_rutas):
     try:
-        with open(nombre_archivo, 'r') as archivo:
+        with open(archivo_rutas, 'r') as archivo:
             lineas = archivo.readlines()
             cola_resultado = CircularQueue()
             for linea in lineas:
@@ -42,30 +51,30 @@ def leer_archivo(nombre_archivo):
                     "puerto_llegar": int(linea[4])
                 }
                 cola_resultado.enqueue(diccionario)
-            
+
             return cola_resultado
-        
+
     except FileNotFoundError:
-        print(f"El archivo {nombre_archivo} no se encuentra.")
+        print(f"El archivo {archivo_rutas} no se encuentra.")
         return None
 
 
 # test
 nombre_del_archivo = 'Conf_5_routers/rutas_R2_v3.txt'
 resultado = leer_archivo(nombre_del_archivo)
-
-# if resultado:
-#      print(resultado)
+if debug:
+    if resultado:
+        print(resultado)
 
 
 # manejo de rutas ==============================================
 
 def check_routes(routes_file_name, destination_address):
     global cola_de_rutas
-    print("cola actual:",cola_de_rutas)
+    if debug:
+        print("cola actual:", cola_de_rutas,"\n")
     if cola_de_rutas.is_empty():
         cola_de_rutas = leer_archivo(routes_file_name)
-        
 
     dest_ip = destination_address[0]
     dest_port = destination_address[1]
@@ -76,23 +85,25 @@ def check_routes(routes_file_name, destination_address):
     while contador < total_rutas:
         primera = cola_de_rutas.get_first()
         red = primera["red"]
-        pto_ini = primera["puerto_inicial"] 
+        pto_ini = primera["puerto_inicial"]
         pto_fin = primera["puerto_final"]
         if dest_ip == red and pto_ini <= dest_port <= pto_fin:
             # encontramos la ruta buscada
             return (primera["ip_llegar"], primera["puerto_llegar"])
-        
-        contador +=1
+
+        contador += 1
 
     # no se encontró una ruta
     return None
 
 
 # test:
-# archivo = "Conf_5_routers/rutas_R2_v3.txt"
-# assert check_routes(archivo,
-#                     ("127.0.0.1", 8884)) == ("127.0.0.1", 8883)
-# assert check_routes(archivo,
-#                     ("127.0.0.1", 8880)) == None
-# assert check_routes(archivo,
-#                     ("127.0.0.1", 8887)) == None
+# precaución: ejecutar este test modifica la cola de rutas global
+if debug:
+    archivo = "Conf_5_routers/rutas_R2_v3.txt"
+    assert check_routes(archivo,
+                        ("127.0.0.1", 8884)) == ("127.0.0.1", 8883)
+    assert check_routes(archivo,
+                        ("127.0.0.1", 8880)) == None
+    assert check_routes(archivo,
+                        ("127.0.0.1", 8887)) == None
